8dd0ac5f41a7f27199adc43381b98976f19de1e2
Brandon <bpayero@usca.edu>
2022-12-03 20:17:41 -0500

    Fixed Bug Report and Contact sections +more

---
+++ js/bugReport.js
@@ -0,0 +1,26 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAb9zglNHMpwfHoQaL8v-jxd_dNN-A7_u4",
+    authDomain: "game-site-d97cf.firebaseapp.com",
+    projectId: "game-site-d97cf",
+    storageBucket: "game-site-d97cf.appspot.com",
+    messagingSenderId: "1058182439081",
+    appId: "1:1058182439081:web:ccc1d2944102c2db9089b8",
+    measurementId: "G-F04G3G3QTW"
+};
+var bugReport = "";
+
+window.onload = function() {
+    firebase.initializeApp(firebaseConfig);
+}
+
+
+
+function sendReport() {
+    bugReport = document.getElementById('report').textContent();
+
+    firebase.firestore().collection('bug').add({
+    report: bugReport
+  });
+}
+
+document.getElementById('submitReport').addEventListener('click', sendReport, false);
